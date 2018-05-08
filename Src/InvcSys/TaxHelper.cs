428c9affe57a48568026ff6f8ac14d0b229b88bc
nauy <d@icloud.com>
2018-05-08 11:18:07 +0800

    处理税额相关计算

---
+++ Src/InvcSys/TaxHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvcSys
+{
+    internal class TaxHelper
+    {
+        /// <summary>
+        /// 计算税额
+        /// </summary>
+        /// <param name="quantity">物品数量</param>
+        /// <param name="price">价格</param>
+        /// <param name="taxRate">税率</param>
+        /// <returns></returns>
+        internal static decimal GetTaxAmount(int quantity, decimal price, decimal taxRate)
+        {
+            decimal amount = decimal.Multiply(quantity, price);
+            //return decimal.Add(amount, decimal.Multiply(amount, taxRate));
+            return decimal.Multiply(amount, taxRate);
+        }
+    }
+}
